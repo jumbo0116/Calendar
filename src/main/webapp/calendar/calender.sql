8ed064ababd79d0dd861a45943843b82f60dee57
jumbo0116 <jumbo0116@gmail.com>
2024-01-18 16:28:30 +0800

    update

--- src/main/webapp/calendar/calender.sql
+++ src/main/webapp/calendar/calender.sql
@@ -22,11 +22,19 @@ CREATE TABLE `diary` (
 
 -- 一月樣本
 insert into calendar.diary(Id,date,name,type,description,account,note)
+values('jumbo0116',20240101,'晚餐',101,80,1,'');
+insert into calendar.diary(Id,date,name,type,description,account,note)
+values('jumbo0116',20240102,'晚餐',101,100,1,'');
+insert into calendar.diary(Id,date,name,type,description,account,note)
+values('jumbo0116',20240103,'晚餐',101,80,1,'');
+insert into calendar.diary(Id,date,name,type,description,account,note)
 values('jumbo0116',20240105,'晚餐',101,300,1,'');
 insert into calendar.diary(Id,date,name,type,description,account,note)
 values('jumbo0116',20240105,'計程車錢',102,600,1,'');
 insert into calendar.diary(Id,date,name,type,description,account,note)
 values('jumbo0116',20240106,'密室逃脫',103,700,1,'好好玩');
+insert into calendar.diary(Id,date,name,type,description,account,note)
+values('jumbo0116',20240106,'公車',102,80,1,'');
 insert into calendar.diary(Id,date,name,type,description,account,note)
 values('jumbo0116',20240106,'牙刷',104,100,1,'');
 insert into calendar.diary(Id,date,name,type,description,account,note)
@@ -39,6 +47,8 @@ insert into calendar.diary(Id,date,name,type,description,account,note)
 values('jumbo0116',20240108,'車禍賠錢',108,5000,1,'');
 insert into calendar.diary(Id,date,name,type,description,account,note)
 values('jumbo0116',20240106,'薪水',201,50000,1,'');
+insert into calendar.diary(Id,date,name,type,description,account,note)
+values('jumbo0116',20240116,'晚餐',101,100,1,'');
 
 -- 二月樣本
 insert into calendar.diary(Id,date,name,type,description,account,note)
